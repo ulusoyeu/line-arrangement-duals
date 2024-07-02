b50e4ce8ecce7cc1c2ff5ab2f30b4ee66be6e451
Emre Ulusoy <mreulusoy03@gmail.com>
2024-07-02 16:49:00 +0100

    feat: add lines from 2 points, line intersections

--- line_arrangements.py
+++ line_arrangements.py
@@ -1,3 +1,7 @@
+import numpy as np
+import sympy as sp
+import random
+
 # A point in R^2
 class Point:
 
@@ -5,12 +9,32 @@ class Point:
         self.x = x
         self.y = y
 
-# Implements the line in an affine linear way - {p | p = (x,y) + b}
+# Implements the line using 3 points to define the line
 class Line:
 
     def __init__(self, p1 : Point, p2 : Point) -> None:
-        self.slope = None 
-        self.offset = None
+        self.p1 = p1
+        self.p2 = p2
+        # Returns an array [a, b, c] that represents the line in format ax + by + c = 0
+        self.linear_format = self.compute_linear_format()
+    
+    def compute_linear_format(self) -> float:
+        equation_matrix = sp.Matrix( np.array([[self.p1.x, self.p1.y, 1], 
+                                     [self.p2.x, self.p2.y, 1]]) )
+        nullspace = equation_matrix.nullspace()
+
+        # Convert all elements to Rational
+        rationals = [sp.Rational(elem) for elem in nullspace[0]]
+
+        # Find the LCM of all denominators
+        denominators = [r.q for r in rationals]
+        lcm_value = sp.lcm(denominators)
+
+        # Scale each element
+        solution = [int(coord * lcm_value) for coord in nullspace[0]]
+
+        return solution
+        
 
 # Creates a regular polygon with n sides
 class RegularPolygon:
@@ -32,6 +56,35 @@ class LineArrangement:
         self.half_edges = None
         self.faces = None
         self.bounding_box = None
+
+    # Returns true if the lines intersect
+    def intersects(self, line1 : Line, line2 : Line) -> bool:
+
+        arrl1 = line1.linear_format
+        arrl2 = line2.linear_format
+
+        M = np.array([[arrl1[0], arrl1[1]],
+               [arrl2[0], arrl2[1]]])
+        
+        if np.linalg.matrix_rank(M) == 2:
+            return True
+        else:
+            return False
+
+    # Returns the intersection point of two lines if it exists
+    def line_intersection(self, line1 : Line, line2 : Line):
+        
+        if not self.intersects(line1, line2):
+            print("The lines do NOT intersect!")
+        
+        arrl1 = l1.linear_format
+        arrl2 = l2.linear_format
+
+        M = np.array([[arrl1[0], arrl1[1]],
+                       [arrl2[0], arrl2[1]]])
+        B = np.array([-1 * arrl1[2], -1 * arrl2[2]])
+
+        return np.linalg.solve(M, B)
     
     # Computes all intersections and picks the largest box that contains all intersections (with extra area)
     def find_bounding_box(self):
@@ -46,4 +99,4 @@ class LineArrangement:
         pass
 
 # IMPORTANT - How to ensure no issues occur in arrangements where the line perfectly crosses through the vertices - do not want numerical errors!
-    
+
